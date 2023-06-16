2654a9b2cfa411b19619c503f6ce5de5e9eaa413
David Machacek <davido.machacek@gmail.com>
2023-06-16 13:50:00 +0200

    init commit

--- src/main/kotlin/cz/sgconsulting/azfunctionsapp/AzFunctionsAppApplication.kt
+++ src/main/kotlin/cz/sgconsulting/azfunctionsapp/AzFunctionsAppApplication.kt
@@ -1,11 +1,12 @@
 package cz.sgconsulting.azfunctionsapp
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+
 
 @SpringBootApplication
 class AzFunctionsAppApplication
 
 fun main(args: Array<String>) {
-	runApplication<AzFunctionsAppApplication>(*args)
+	SpringApplication.run(AzFunctionsAppApplication::class.java, *args)
 }
